7f052efb5ad6ecdefda5dac938d252cf3e01fb16
SameeraK <sikumarasinghe@gmail.com>
2024-08-24 08:18:18 +1200

    initial commit

---
+++ src/ToggleSounds.js
@@ -0,0 +1,15 @@
+import { memo } from "react";
+
+const ToggleSounds = memo(function ToggleSounds({ allowSound, setAllowSound }) {
+  console.log("rendering ToggleSounds....");
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+});
+
+export default ToggleSounds;
